0f76c652060a1d14f4b2e5fce24d85c81f62ef01
Marian Leica <marianleica@lesformidables.club>
2025-02-11 18:07:26 +0200

    Update spin-srvenv.sh complete with ACR deployment

--- srv/spin-srvenv.sh
+++ srv/spin-srvenv.sh
@@ -39,4 +39,33 @@ sleep 1
 #echo ""
 #echo "Install kubectl locally, if needed: az aks install-cli"
 #echo ""
+
+#########A##################################################################################################
+
+echo "Creating an Azure Container Registry (ACR)"
+sleep 1
+
+# Setting variables
+timestamp=$(date '+%Y/%m/%d-%H:%M UTC%z')
+scenario="azacr-public"
+suffix=$((10000 + RANDOM % 99999))
+rg="mailman"
+location="uksouth"
+acr="azacr-public-$suffix"
+acrpath=$acr.azurecr.io
+
+# Create the ACR resource
+az acr create -n $acr -g $rg --sku Standard
+
+# Login to ACR in azcli shell
+# az acr login --name $acr
+
+# Add a basic image to the repository for testing
+# az acr import -n $acr --source docker.io/library/hello-world:latest -t $acrpath:test1
+
+############################################################################################################
+
+
+
+
 read -n 1 -srp "Press any key to continue..." && echo ""
